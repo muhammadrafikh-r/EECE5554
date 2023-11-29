9b411417fb35ac21402b2196dc5d8ab5920a2ec9
muhammadrafikh-r <muhammadrafikh.r@northeastern.edu>
2023-11-28 20:29:39 -0500

    LAB4 Initial Commit

---
+++ LAB4/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rayyan/catkin_ws/EECE5554/LAB4/src
+# Build directory: /home/rayyan/catkin_ws/EECE5554/LAB4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
